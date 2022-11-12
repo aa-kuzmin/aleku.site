c01c6728067b7e0fc0a0300b03620a6fe54c77c6
Aleksei Kuzmin <aa-kuzmin@mail.ru>
2022-11-12 15:59:10 +0400

    Switch from initial form to main completed

--- js/init.js
+++ js/init.js
@@ -1,23 +1,42 @@
 var initLang = "RU";
 
 function onInitClickContinue(event) {
-    alert("В стадии разработки");
+
+  this.removeEventListener("click", onInitClickContinue);
+
+  elems = document.querySelectorAll("section");
+  for (el of elems) el.style["content-visibility"] = "visible";
+  
+  elems = document.querySelectorAll("header");
+  for (el of elems) el.style["content-visibility"] = "visible";
+  
+  elems = document.querySelectorAll("footer");
+  for (el of elems) el.style["content-visibility"] = "visible";
+
+  el = document.querySelector("section[class='init']");
+  el.remove();
+
 }
 
 function onInitClickLang(event) {
+
   if (initLang == this.innerText) return;
+
   initLang = this.innerText;
+
   div = document.querySelector("div[class='init']");
   button = document.querySelector("button[class='init_continue']");
   buttonBackground = getComputedStyle(button)["background-color"];
   sectionBackground = getComputedStyle(document.querySelector("section[class='init']"))["background-color"];
   buttonColor = getComputedStyle(document.querySelector("button[class='init_continue']"))["color"];
   sectionColor = getComputedStyle(document.querySelector("section[class='init']"))["color"];
+
   elems = document.querySelectorAll("div[class='init_lang_switch']");
   for (el of elems) {
     el.style.setProperty("background-color", el["innerText"] == initLang ? buttonBackground : sectionBackground);
     el.style.setProperty("color", el["innerText"] == initLang ? buttonColor : sectionColor);
   };
+
   switch (initLang){
     case "RU":
     div.innerHTML = "<p style='text-align: center; padding: 5px;'>Добрый день!<p><p>Это сайт-резюме. Если вы не работодатель в поиске новых сотрудников, скорее всего ничего интересного вы здесь для себя не найдёте.<p>";
@@ -38,6 +57,7 @@ function onInitClickLang(event) {
   }
 }
 
+document.querySelector("section[class='init']").style["content-visibility"] = "visible";
 div = document.querySelector("div[class='init']");
 button = document.querySelector("button[class='init_continue']");
 button.addEventListener("click", onInitClickContinue);
